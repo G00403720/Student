338a4dfe462e45254be9c08fbe11f011e08213e7
G00403720 <G00403720@atu.ie>
2022-10-24 11:24:22 +0100

    Added new file called Student3

---
+++ src/main/java/ie/atu/week5/Student3.java
@@ -0,0 +1,31 @@
+package ie.atu.week5;
+
+public class Student3 {
+   private String name;
+   private double email;
+   private double course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getEmail() {
+        return email;
+    }
+
+    public void setEmail(double email) {
+        this.email = email;
+    }
+
+    public double getCourse() {
+        return course;
+    }
+
+    public void setCourse(double course) {
+        this.course = course;
+    }
+}
