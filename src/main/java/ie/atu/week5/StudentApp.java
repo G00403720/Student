b148eb9b4a75a3e0936b14b6ba79f1b5aa110247
G00403720 <G00403720@atu.ie>
2022-10-24 10:03:51 +0100

    Prompt user for name

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -1,4 +1,14 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter your student name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("You entered : " + name);
+
+    }
 }
